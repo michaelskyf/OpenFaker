3bb4d9ed8d9f6272ad3b21cb60342c286bf82bfa
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2023-08-15 13:28:35 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("com.google.code.gson:gson:2.10.1")
     compileOnly("de.robv.android.xposed:api:82")
 }
