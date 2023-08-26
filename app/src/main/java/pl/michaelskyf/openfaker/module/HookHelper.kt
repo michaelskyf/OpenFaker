dee89f83d0fcf6778eaad04570f5f2adc3d52c5c
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2023-08-26 12:01:34 +0200

    Shorten HookHelper

--- app/src/main/java/pl/michaelskyf/openfaker/module/HookHelper.kt
+++ app/src/main/java/pl/michaelskyf/openfaker/module/HookHelper.kt
@@ -4,17 +4,7 @@ import java.lang.reflect.Field
 import java.lang.reflect.Method
 
 abstract class HookHelper {
-    open fun findMethod(className: String, classLoader: ClassLoader, methodName: String, vararg parameterTypes: Any): Method? {
-
-        return null
-    }
-
-    open fun hookMethod(method: Method, callback: Hook.MethodHookHandler) {
-
-    }
-
-    open fun findField(classType: Class<*>, fieldName: String): Field? {
-
-        return null
-    }
+    open fun findMethod(className: String, classLoader: ClassLoader, methodName: String, vararg parameterTypes: Any): Method? = null
+    open fun hookMethod(method: Method, callback: Hook.MethodHookHandler) = Unit
+    open fun findField(classType: Class<*>, fieldName: String): Field? = null
 }
